21503227bad208fb2f833dbc7f2b4610b03fd031
mzxdream@gmail.com <mzxdream@gmail.com>
2013-10-08 13:47:38 +0800

    modify ConfDefine.h

--- src/client/ConfDefine.h
+++ src/client/ConfDefine.h
@@ -2,7 +2,16 @@
 #define _CONFDEFINE_H_
 
 #define CONF_DIR "./Conf/"
-#define CONF_LOGIN "./Conf/LoginConf.ini"
-#define CONF_LOGININFOSET CONF_DIR##"LoginInfoSetConf.ini"
+#define CONF_PATH(x) CONF_DIR#x
+
+#define CONF_LOGIN CONF_PATH(Login.ini)
+#define CONF_LOGIN_USERINFO "userinfo"
+#define CONF_LOGIN_USERINFO_ID "userid"
+#define CONF_LOGIN_USERINFO_PWD "password"
+#define CONF_LOGIN_USERINFO_RMBPWD "rememberpwd"
+#define CONF_LOGIN_USERINFO_AUTOLOGIN "autologin"
+#define CONF_LOGIN_SERVERINFO "serverinfo"
+#define CONF_LOGIN_SERVERINFO_ADDR "address"
+#define CONF_LOGIN_SERVERINFO_PORT "port"
 
 #endif
