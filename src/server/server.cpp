d6f82c6e42e477521c48fe0dcc12b90e0f5d816b
mzxdream@gmail.com <mzxdream@gmail.com>
2013-10-16 22:38:11 +0800

    modify server

--- src/server/server.cpp
+++ src/server/server.cpp
@@ -1,6 +1,5 @@
 #include <stdlib.h>
 #include <stdio.h>
-#include <errno.h>
 #include <string.h>
 #include <unistd.h>
 #include <netdb.h>
@@ -9,39 +8,56 @@
 #include <sys/types.h>
 #include <arpa/inet.h>
 
-#define SERVER_PORT 1018
-#define MAX_MSG_SIZE 1024
+#define SERVER_PORT 3230
+#define LISTEN_LEN 100
+
+bool SetNonBlock(int nSock)
+{
+    int nFlags = 0;
+    int nTmp = 0;
+    if ((nFlags = fcnt(nSock, F_GETFL, 0)) == -1)
+    {
+	return false;
+    }
+    nFlags |= O_NONBLOCK;
+    if (fcntl(nSock, F_SETFL, nFlags) == -1)
+    {
+	return false;
+    }
+    return true;
+}
 
 int main(int argc, char *argv[])
 {
-    int sockfd;
-    struct sockaddr_in addr;
-    sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    if (sockfd == -1)
+    int nSock = 0;
+    if ((nSock = socket(AF_INET, SOCK_STREAM, 0)) < 0)
     {
 	return 0;
     }
-    bzero(&addr, sizeof(struct sockaddr_in));
-    addr.sin_family = AF_INET;
-    addr.sin_addr.s_addr = htonl(INADDR_ANY);
-    addr.sin_port = htons(SERVER_PORT);
-    if (bind(sockfd, (struct sockaddr *)(&addr), sizeof(struct sockaddr)) == -1)
+    struct sockaddr_in servAddr;
+    bzero(&servAddr, sizeof(servAddr));
+    servAddr.sin_family = AF_INET;
+    servAddr.sin_port = htnos(SERVER_PORT);
+    servAddr.sin_addr.s_addr = htons(INADDR_ANY);
+    if (bind(nSock, (struct sockaddr *)&servAddr, sizeof(servAddr)) < 0)
     {
 	return 0;
     }
-    if (listen(sockfd, 5) == -1)
+    if (!SetNonBlock(nSock))
     {
 	return 0;
     }
-    while (true)
+    if (listen(nSock, LISTEN_LEN) < 0)
     {
-	int acceptfd = accept(sockfd, NULL, NULL);
-	if (acceptfd <0)
-	{
-	    continue;
-	}
-	
+	return 0;
     }
+    int efd = 0;
+    if ((efd = epoll_create1(0)) == -1)
+    {
+	return 0;
+    }
+    struct epoll_event event;
+    struct epoll_event *events;
     return 0;
     
 }
