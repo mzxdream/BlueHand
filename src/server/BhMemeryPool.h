0587bf47294572468ff2e5ae15364e8b6e9f5ba4
mzxdream@gmail.com <mzxdream@gmail.com>
2013-11-09 00:01:45 +0800

    add BhMemeryPool

--- src/server/BhMemeryPool.h
+++ src/server/BhMemeryPool.h
@@ -6,17 +6,18 @@ class BhMemeryPool
 public:
     explicit BhMemeryPool(unsigned uBlockLen);
     ~BhMemeryPool();
-    BhMemeryPool(const BhMemeryPool &obj);
-    BhMemeryPool& operator=(const BhMemeryPool &obj);
+    BhMemeryPool(const BhMemeryPool &) = delete;
+    BhMemeryPool& operator=(const BhMemeryPool &) = delete;
 public:
     void Clear();
     unsigned Length() const;
-    bool Read(void *pBuf, int nLength);
-    void Write(const void *pBuf, int nLength);
-    void Free(int nLength);
+    bool Read(void *pBuf, unsigned uLen);
+    void Write(const void *pBuf, unsigned uLen);
+    void Free(unsigned uLen);
 private:
     unsigned m_uBlockLen;//每个分块长度
-    unsigned m_uCurPos;//当前位于第一分块的位置
+    unsigned m_uBeginPos;//当前位于第一分块的位置
+    unsigned m_uEndPos;//当前位于最后一个分块的位置
     unsigned m_uLength;//总长度
     std::list<void *> m_blockList;
 };
